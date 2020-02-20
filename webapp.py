671632fb13532aa0924b6772e20e9e180861be62
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2020-02-20 09:51:14 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -23,11 +23,11 @@ github = oauth.remote_app(
     consumer_key=os.environ['GITHUB_CLIENT_ID'], 
     consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
     request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
-    base_url='https://api.github.com/',
+    base_url='http://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',  
-    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+    access_token_url='http://github.com/login/oauth/access_token',  
+    authorize_url='http://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
 
